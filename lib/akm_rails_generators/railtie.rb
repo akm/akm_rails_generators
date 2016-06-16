58e0f16753f813b6161f50189b021feebc9ba536
akm <akm2000@gmail.com>
2016-06-16 18:24:31 +0900

    Insert lib/templates to  Rails::Generators.templates_path when generators work

--- lib/akm_rails_generators/railtie.rb
+++ lib/akm_rails_generators/railtie.rb
@@ -3,11 +3,9 @@ require 'akm_rails_generators'
 module AkmRailsGenerators
   class Railtie < Rails::Railtie
 
-    config.generators do |g|
-      puts "=" * 100
-      puts g.templates
-      g.templates.unshift File::expand_path('../../templates', __FILE__)
+    generators do |g|
+      Rails::Generators.templates_path.unshift(File::expand_path('../../templates', __FILE__))
     end
-  
+
   end
 end
